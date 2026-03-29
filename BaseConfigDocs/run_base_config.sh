5b53a171be5e08de7c35c0038acf45e8610471a2
Daniel Louis <Llouis5314@gmail.com>
2026-03-29 19:48:54 -0400

    Update run_base_config.sh

--- BaseConfigDocs/run_base_config.sh
+++ BaseConfigDocs/run_base_config.sh
@@ -14,20 +14,25 @@ source /BaseConfigDocs/source/install_openManage.sh
 source /BaseConfigDocs/source/config_firewall.sh
 source /BaseConfigDocs/source/create_video00.sh
 source /BaseConfigDocs/source/v8_install_vcs.sh
-source /BaseConfigDocs/source/update_hostname.sh
 source /BaseConfigDocs/source/server_info_output.sh
 source /BaseConfigDocs/source/base_config_last_steps.sh
+source /BaseConfigDocs/source/setup_network_config.sh
+source /BaseConfigDocs/source/local_grafana_installer.sh
+source /BaseConfigDocs/source/tailscale_install.sh 
 
 while true; do
   echo "Please select the option from the following."
   echo "  1. Install Dell Open Manage"
-  echo "  2. Set Firewall"
-  echo "  3. Create the Video Array"
-  echo "  4. Update Hostname"
-  echo "  5. Install v8 of the Asentry Software"
-  echo "  6. Last Steps"
-  echo "  7. Print Server Info"
-  echo "  8. Exit"
+  echo "  2. Set Network to DHCP"
+  echo "  3. Set Firewall"
+  echo "  4. Create the Video Array"
+  echo "  5. Update Hostname"
+  echo "  6. Install v8 of the Asentry Software"
+  echo "  7. Last Steps"
+  echo "  8. Print Server Info"
+  echo "  9. Install a Local Instance of Grafana"
+  echo "  10. Install Tailscale"
+  echo "  11. Exit"
   read -p "Choose an option: " answer 
 
   case $answer in 
@@ -35,30 +40,42 @@ while true; do
       echo "Installing Open Manage"
       install_openManage_main 
       ;;
-    2) 
+    2)
+      setup_network_config
+      ;;
+    3) 
       echo "Settin Firewall"
       config_firewall_main 
       ;;
-    3) 
+    4) 
       echo "Creating video00"
       create_video00_main
       ;;
-    4) 
+    5) 
       echo "Updating Hostname"
       update_hostname_main
       ;;
-    5) 
+    6) 
       echo "Installing v8"
       v8_install_vcs_main
       ;;
-    6) 
+    7) 
       echo "Starting Last Steps"
       base_config_last_steps_main
       ;;
-    7) 
+    8) 
+      echo "Printing Server Info"
       server_info_output_main
       ;;
-    8) 
+    9)
+      echo "Installing Local Grafana Instance"
+      local_grafana_installer
+      ;;
+    10)
+      echo "Installing Tail Scale"
+      tailscale_install 
+      ;;
+    11) 
       echo "Exiting..."
       exit 1
       ;;
